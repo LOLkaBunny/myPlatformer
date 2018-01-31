6cd3c43dd21d8f01a89ed214828667f4d633d959
LOLka <anastasia-1@i.ua>
2018-01-31 22:40:29 +0200

    class walkingEnemy

---
+++ Assets/Scripts/walkingEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkingEnemy : MonoBehaviour {
+
+    public float speed = 7f;
+    float direction = -1f;
+    private Rigidbody2D rigidbody2D;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = new Vector2(speed * direction, rigidbody2D.velocity.y);
+        transform.localScale = new Vector3(direction, 1, 1);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Wall")
+            direction *= -1f;
+    }
+}
